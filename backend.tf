4f3e9bd4f144a4ee6f7a081516a4f7c565b98236
Ravi <ravi@vytec.com>
2024-09-05 10:19:30 +0530

    updated

--- backend.tf
+++ backend.tf
@@ -4,5 +4,9 @@ terraform {
     storage_account_name = "tfbackendstgacc030924"                      
     container_name       = "devtfstate"                       
     key                  = "terraform.tfstate"
+    client_id       = "clientid"
+    client_secret   = "clientsecret"
+    tenant_id       = "tenantid"
+    subscription_id = "subscriptionid"
   }
 }
